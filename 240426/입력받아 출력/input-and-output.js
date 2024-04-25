049fb1379e98cdfcd02a417e5ee2621c6378c34d
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 07:50:51 +0900

    Title: 입력받아 출력, Time: 212ms, Memory: 14MB, Status: Passed - CodeTree

--- 240426/입력받아 출력/input-and-output.js
+++ 240426/입력받아 출력/input-and-output.js
@@ -1,4 +1,3 @@
 const fs = require("fs");
 let s = fs.readFileSync(0).toString().split("\n");
-console.log(Number(s[0]));
-console.log(Number(s[1]));
+console.log(Number(s[0]) + " " + Number(s[1]));
