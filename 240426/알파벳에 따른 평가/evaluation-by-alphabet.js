648d3d6c639000be26e413ed479f757eae51bf2f
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 08:08:43 +0900

    Title: 알파벳에 따른 평가, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240426/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 240426/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 
 let s = fs.readFileSync(0).toString();
 
-if (s == "S") {
+if (s === "S") {
     console.log("Superior");
-} else if (s == "A") {
+} else if (s === "A") {
     console.log("Excellent");
-} else if (s == "B") {
+} else if (s === "B") {
     console.log("Good");
-} else if ("C" == s) {
+} else if ("C" === s) {
     console.log("Usually");
-} else if (s == "D") {
+} else if (s === "D") {
     console.log("Effort");
 } else {
     console.log("Failure");
