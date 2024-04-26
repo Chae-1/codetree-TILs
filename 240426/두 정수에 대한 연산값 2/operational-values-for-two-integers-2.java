5d34b3b82dd9e81b047915df28e4c61a248886bc
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 13:52:29 +0900

    Title: 두 정수에 대한 연산값 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240426/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+
+    static class IntHolder {
+        int value;
+
+        IntHolder(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        IntHolder aHolder = new IntHolder(a);
+        IntHolder bHolder = new IntHolder(b);
+        lessNumPlusTwentyAndGraterNumMultiple(aHolder, bHolder);
+
+        System.out.println(aHolder.value + " " +bHolder.value)
+    }
+
+    public static void lessNumPlusTwentyAndGraterNumMultiple(IntHolder aHolder, IntHolder bHolder) {
+        if (aHloder.value > bHloder.value) {
+            aHolder.value += 10;
+            bHolder.value *= 2;
+            return;
+        }
+        
+        aHolder.value *= 2;
+        bHolder.value += 10;
+    }
+}
