13ced109409772a58a123415647630c2ea59d1e2
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 14:08:25 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 151ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240426/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    static String s;
+    static String substring;
+    static int matchCount = 0;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+        substring = sc.next();
+        int index = findSubSringIndex();
+        System.out.println(index);
+    }
+
+    public static int findSubSringIndex() {
+        int left = 0;
+        while (left < s.length()) {
+            if (isMatch(left)) {
+                return left;
+            }
+            left = matchCount > 0 ? left + matchCount : left + 1;
+        }
+        return -1;
+    }
+
+    public static boolean isMatch(int left) {
+        for (int i = 0; i < substring.length(); i++) {
+            if (s.charAt(left + i) != substring.charAt(i)) {
+                return false;
+            }
+            matchCount++;
+        }
+        return true;
+    }
+}
