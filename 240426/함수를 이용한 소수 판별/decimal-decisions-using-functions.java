761b74f43ebe582d70f9e001d376d2cea1b1b665
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 10:20:39 +0900

    Title: 함수를 이용한 소수 판별, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240426/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240426/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -14,7 +14,10 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-        for (int i = 2; i < n / 2; i++) {
+        if (n == 1)
+            return false;
+
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0)
                 return false;
         }
