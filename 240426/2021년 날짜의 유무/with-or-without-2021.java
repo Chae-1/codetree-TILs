0801ccca0f155b715d9451faeb01b495a08972cb
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 12:27:41 +0900

    Title: 2021년 날짜의 유무, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240426/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (isExistDayIn2021(m, d)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isExistDayIn2021(int m, int d) {
+        // m이 유효한지 확인
+        // m은 1부터 12까지 유효
+        if (!validateMonth(m)) {
+            return false;
+        }
+
+        if (!validateDayInMonth(m, d)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validateMonth(int m) {
+        if (m > 13)
+            return false;
+        return true;
+    }
+
+    public static boolean validateDayInMonth(int m, int d) {
+        if (d > 31)
+            return false;
+        
+        if (m == 2 && d > 28) {
+            return false;
+        }
+
+        if (m == 4 || m == 6 || m == 9 || m == 11) {
+            if (d > 30)
+                return false;
+        }
+        return true;
+    }
+}
