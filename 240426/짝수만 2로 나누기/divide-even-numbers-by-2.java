2c19440f26edba067a1f03df45643af4981367ef
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-26 13:29:31 +0900

    Title: 짝수만 2로 나누기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240426/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        evenDivide(arr);
+        printArr(arr);
+    }
+
+    public static void evenDivide(int[] arr) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0)
+                arr[i] = arr[i] / 2;  
+        }
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print(arr[i] + " "); 
+        }
+        System.out.print(arr[i]);
+        
+    }
+}
