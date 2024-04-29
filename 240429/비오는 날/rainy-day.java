4ea9b8aeab37654cb70a77acb7241293cc80069f
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-29 14:27:07 +0900

    Title: 비오는 날, Time: 347ms, Memory: 17MB, Status: Passed - CodeTree

--- 240429/비오는 날/rainy-day.java
+++ 240429/비오는 날/rainy-day.java
@@ -1,6 +1,10 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.*;
+
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         WeatherInfo[] weatherInfos = new WeatherInfo[n];
@@ -11,57 +15,27 @@ public class Main {
             weatherInfos[i] = new WeatherInfo(date, weather, day);
         }
 
-        WeatherInfo info = weatherInfos[0];
-        for (int i = 1; i < n; i++) {
-            if (isSwap(info, weatherInfos[i])) {
-                info = weatherInfos[i];
-            }
-        }
-        System.out.println(info.date + " " + info.day + " " + info.weather);
-    }
-
-    private static boolean isSwap(WeatherInfo info, WeatherInfo weatherInfo) {
-        if (!info.weather.equals("Rain")) {
-            return true;
-        }
-
-        if (!weatherInfo.weather.equals("Rain")) {
-            return false;
-        }
-
-        String[] prevDate = weatherInfo.date.split("-");
-        int prevYear = Integer.parseInt(prevDate[0]);
-        int prevMonth = Integer.parseInt(prevDate[1]);
-        int prevDay = Integer.parseInt(prevDate[2]);
-
-        String[] date = weatherInfo.date.split("-");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-
-        if (prevYear > year) {
-            return true;
-        } else if (prevYear == year) {
-            if (prevMonth > month) {
-                return true;
-            } else if (prevMonth == month) {
-                if (prevDay > day) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        System.out.println(Arrays.stream(weatherInfos)
+                .filter(w -> w.weather.equals("Rain"))
+                .sorted(Comparator.comparing(w -> w.date))
+                .findFirst()
+                .get());
     }
 
     static class WeatherInfo {
-        String date;
+        LocalDate date;
         String weather;
         String day;
 
         public WeatherInfo(String date, String weather, String day) {
-            this.date = date;
+            this.date = LocalDate.parse(date);
             this.weather = weather;
             this.day = day;
         }
-    }    
+
+        @Override
+        public String toString() {
+            return date + " " + day + " " + weather;
+        }
+    }
 }
