0d4ae552e0f698eaa27faf215cef6db62ccdbdde
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-29 11:52:40 +0900

    Title: Next Level, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240429/Next Level/next-level.java
@@ -0,0 +1,27 @@
+public class Main {
+        static class User {
+        String id;
+        int level;
+
+        public User(String id, int level) {
+            this.id = id;
+            this.level = level;
+        }
+
+        @Override
+        public String toString() {
+            return "user " + id + " " + "lv " + level; 
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        int level = sc.nextInt();
+        User codetree = new User("codetree", 10);
+        User user = new User(id, level);
+        System.out.println(codetree);
+        System.out.println(user);
+    }
+}
