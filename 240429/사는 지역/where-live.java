e336c7737baab36e12b71c60b7564a681be60072
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-29 12:28:36 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240429/사는 지역/where-live.java
@@ -0,0 +1,30 @@
+public class Main {
+    static class Member {
+        String name;
+        String addr;
+        String city;
+
+        public Member(String name, String addr, String city) {
+            this.name = name;
+            this.addr = addr;
+            this.city = city;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Member[] members = new Member[n];
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            String addr = sc.next();
+            String city = sc.next();
+            members[i] = new Member(name, addr, city);;
+        }
+
+        Arrays.sort(members, Comparator.comparing(m -> m.name));
+        System.out.println("name " + members[n-1].name);
+        System.out.println("addr " + members[n-1].addr);
+        System.out.println("city " + members[n-1].city);
+    }
+}
