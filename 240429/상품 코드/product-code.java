2a62982bd4a36f54b18d1ace7ec7c407a09d8cd4
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-29 12:10:09 +0900

    Title: 상품 코드, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240429/상품 코드/product-code.java
@@ -0,0 +1,22 @@
+public class Main {
+    static class Product {
+        int code;
+        String name;
+
+        public Product(int code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int code = sc.nextInt();
+        Product codetree = new Product(50, "codetree");
+        Product product = new Product(code, name);
+
+        System.out.println("product " + codetree.code + " is " + codetree.name);
+        System.out.println("product " + product.code + " is " + product.name);
+    }
+}
