40442905fe02b5241d3e3cce622658040f6f7f7c
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-04-30 16:46:55 +0900

    Title: 십진수와 이진수 2, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240430/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main {
+public static int toBinary(int n) {
+        int[] arr = new int[20];
+        int cnt = 0;
+        while (true) {
+            if (n < 2) {
+                arr[cnt++] = n;
+                break;
+            }
+            arr[cnt++] = n % 2;
+            n /= 2;
+        }
+
+        int result = 0;
+        for (int i = cnt - 1; i >= 0; i--) {
+            result = result * 10 + arr[i];
+        }
+        return result;
+    }
+
+    public static int toDecimal(int b) {
+        int result = 0;
+        int cnt = 1;
+        while (b != 0) {
+            result += cnt * (b % 10);
+            b /= 10;
+            cnt *= 2;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = toDecimal(n) * 17;
+        System.out.println(toBinary(i));
+    }
+}
