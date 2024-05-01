25a814bad4a2b3d4f88ac965ab7126b83184ed30
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-01 17:25:03 +0900

    Title: 만나는 그 순간, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240501/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+    // a, b가 이동한 거리는 똑같다.
+    // n, m 만큼 방향이 결정된다.
+    static int[] A = new int[2_000_001];
+    static int[] B = new int[2_000_001];
+    static int[] dir = new int[] {-1, 1};
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int m = sc.nextInt();
+       int time = 0;
+       for (int i = 0; i < n; i++) {
+            String direc = sc.next();
+            int step = sc.nextInt();
+            int dir = direc.equals("L") ? -1 : 1;
+            for (int j = 0; j < step; j++) {
+                A[++time] = A[time - 1] + dir;
+            }
+        }
+        
+        time = 0;
+        for (int i = 0; i < m; i++) {
+            String direc = sc.next();
+            int step = sc.nextInt();
+            int dir = direc.equals("L") ? -1 : 1;
+            for (int j = 0; j < step; j++) {
+                B[++time] = B[time - 1] + dir;
+            }
+        }
+
+        for (int i = 1; i < 2_000_001; i++) {
+            if (A[i] == B[i]) {
+                System.out.println(i);
+                break; 
+            }
+        }
+    }
+}
