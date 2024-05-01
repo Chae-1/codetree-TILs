421ee7c8f3c1d66214811294273c61429c047655
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-01 20:13:53 +0900

    Title: 왔다 갔던 구역 2, Time: 98ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240501/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240501/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,39 +1,81 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class Main {
-    private static int MAX_LEN = 2001;
-    private static int[] arr = new int[MAX_LEN];
 
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+    private static class Location {
+        int x;
 
-        int current = 1000;
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            String command = sc.next();
-            if (command.equals("L")) {
-                while (x != 0) {
-                    arr[current--]++;
-                    x--;
-                }
-            } else {
-                while (x != 0) {
-                    arr[current++]++;
-                    x--;
-                }
+        public Location(int x) {
+            this.x = x;
+        }
+    }
+
+    private static class Command {
+        int n;
+        String direct;
+
+        public Command(int n, String direct) {
+            this.n = n;
+            this.direct = direct;
+        }
+    }
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static int N;
+    private static Map<Integer, Integer> map;
+    private static List<Command> commands;
+    private static Location location;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+
+    private static void init() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        map = new HashMap<>();
+        commands = new ArrayList<>();
+        location = new Location(0);
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            commands.add(new Command(Integer.parseInt(st.nextToken()), st.nextToken()));
+        }
+    }
+
+    private static void solve() {
+        for (Command command : commands) {
+            move(command);
+        }
+    }
+
+    private static void move(Command command) {
+        if (command.direct.equals("R")) {
+            for (int i = 0; i < command.n; i++) {
+                location.x++;
+                map.put(location.x, map.getOrDefault(location.x, 0) + 1);
+            }
+        } else {
+            for (int i = 0; i < command.n; i++) {
+                location.x--;
+                map.put(location.x, map.getOrDefault(location.x, 0) + 1);
             }
         }
+    }
 
+    private static void print() {
         int count = 0;
-        for (int i = 0; i <= 2000; i++) {
-            int area = 0;
-            while (arr[i] >= 2) {
-                area++;
-                i++;
-            }
-            if (area > 0) {
-                count += area;
+        for (Integer value : map.values()) {
+            if (value > 1) {
+                count++;
             }
         }
         System.out.println(count);
