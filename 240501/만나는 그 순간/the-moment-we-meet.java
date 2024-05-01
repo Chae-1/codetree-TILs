b02b76b4c5b601f78cd011e60c44de9e02f5fed2
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-01 17:54:25 +0900

    Title: 만나는 그 순간, Time: 270ms, Memory: 38MB, Status: Passed - CodeTree

--- 240501/만나는 그 순간/the-moment-we-meet.java
+++ 240501/만나는 그 순간/the-moment-we-meet.java
@@ -6,40 +6,43 @@ public class Main {
     // n, m 만큼 방향이 결정된다.
     static int[] A = new int[2_000_001];
     static int[] B = new int[2_000_001];
-    static int[] dir = new int[] {-1, 1};
+
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int m = sc.nextInt();
-       int time = 0;
-       int stopCount = 0;
-       for (int i = 0; i < n; i++) {
-            String direc = sc.next();
-            int step = sc.nextInt();
-            int dir = direc.equals("L") ? -1 : 1;
-            for (int j = 0; j < step; j++) {
-                A[++time] = A[time - 1] + dir;
-                stopCount++;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int totalTime = 0;
+        int time = 1;
+        for (int i = 0; i < n; i++) {
+            String dir = sc.next();
+            int t = sc.nextInt();
+            int step = dir.equals("L") ? -1 : 1;
+            for (int j = 0; j < t; j++) {
+                A[time] = A[time - 1] + step;
+                time++;
             }
+            totalTime += t;
         }
-        
-        time = 0;
+
+        time = 1;
         for (int i = 0; i < m; i++) {
-            String direc = sc.next();
-            int step = sc.nextInt();
-            int dir = direc.equals("L") ? -1 : 1;
-            for (int j = 0; j < step; j++) {
-                B[++time] = B[time - 1] + dir;
+            String dir = sc.next();
+            int t = sc.nextInt();
+            int step = dir.equals("L") ? -1 : 1;
+            for (int j = 0; j < t; j++) {
+                B[time] = B[time - 1] + step;
+                time++;
             }
         }
-        for (int i = 1; i < stopCount; i++) {
+
+        int meet = -1;
+        for (int i = 1; i <= totalTime; i++) {
             if (A[i] == B[i]) {
-                System.out.println(i);
-            } else {
-                if (i == stopCount - 1) {
-                    System.out.println(-1);
-                }
+                meet = i;
+                break;
             }
         }
+
+        System.out.println(meet);
     }
 }
