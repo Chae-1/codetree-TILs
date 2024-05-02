d444f81fe831a96553aea3f53edabc32f8875066
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-02 16:41:00 +0900

    Title: 격자 위의 편안한 상태, Time: 277ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240502/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] arr;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        arr = new int[n+1][n+1];
+
+        // 색칠할 횟수
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            arr[r][c] = 1;
+            if (isComportable(r, c, n)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            } 
+        }
+    }
+
+    private static boolean isComportable(int r, int c, int n) {
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + c;
+            int ny = dy[i] + r;
+            if (ny < 1 || ny > n || nx < 1 || nx > n) {
+                continue;
+            }
+
+            if (arr[ny][nx] == 1)
+                count++;
+        }
+        return count >= 3;
+    }
+}
