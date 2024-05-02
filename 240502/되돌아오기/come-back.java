80b498a942fc5f8d31d16898874ab5a6654542be
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-02 16:11:56 +0900

    Title: 되돌아오기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240502/되돌아오기/come-back.java
+++ 240502/되돌아오기/come-back.java
@@ -1,19 +1,30 @@
 import java.util.*;
 public class Main {
-    // N, S, E, W
-    //
+    
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
     static int[][] arr;
+    static int elapsedTime;
+    static int x;
+    static int y;
+
+    private static boolean move(int dir, int dist) {
+        while (dist-- > 0) {
+            x += dx[dir];
+            y += dy[dir];
+            
+            elapsedTime++;
+            if (x == 0 && y == 0)
+                return true;      
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int nx = 1000;
-        int ny = 1000;
-        int time = 0;
         int firstTimeToZero = -1;
-        arr = new int[2001][2001];
-
+       
         for (int i = 0; i < n; i++) {
             char dir = sc.next().charAt(0);
             int dist = sc.nextInt();
@@ -28,13 +39,10 @@ public class Main {
                 dirNum = 3;
             }
 
-            for (int j = 0; j < dist; j++) {
-                arr[ny][nx] = time++;
-                nx += dx[dirNum];
-                ny += dy[dirNum];
-                if (i != 0 && ny == 1000 && nx == 1000 && firstTimeToZero == -1) {
-                    firstTimeToZero = time;
-                }
+            boolean done = move(dirNum, dist);
+            if (done) {
+                firstTimeToZero = elapsedTime;
+                break;
             }
         }
         System.out.println(firstTimeToZero);
