c95c91194cfaeb06cc3c9ab5799daf4986073b6e
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-03 08:22:29 +0900

    Title: 거울에 레이저 쏘기 2, Time: 228ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240503/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240503/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 // / 는
 public class Main {
+
     // 하, 좌, 상, 우
     static int[] dx = {0, -1, 0, 1};
     static int[] dy = {-1, 0, 1, 0};
@@ -13,10 +14,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        arr = new char[n][];
+        arr = new char[n][n];
         for (int i = 0; i < n; i++) {
             String line = sc.next();
-            arr[i] = new char[line.length()];
             for (int j = 0; j < line.length(); j++) {
                 arr[i][j] = line.charAt(j);
             }
@@ -25,7 +25,7 @@ public class Main {
         int k = sc.nextInt();
         // 시작 위치를 구한다.
         findStartPosition(k, n);
-        dirNum = k / 4; // 시작 방향
+        dirNum = (k - 1) / n; // 시작 방향
         while (inRange(x, y, n)) {
             if (arr[y][x] == '\\') {
                 if (dirNum == 0 || dirNum == 2) {
@@ -40,8 +40,6 @@ public class Main {
                 } else {
                     dirNum = (dirNum - 1 + 4) % 4;
                 }
-            } else {
-
             }
             x += dx[dirNum];
             y += dy[dirNum];
@@ -62,7 +60,7 @@ public class Main {
                 x = nx;
                 y = ny;
             } else {
-                dirNum = (dirNum + 1) % 4;
+                dirNum = (dirNum - 1 + 4) % 4;
             }
         }
     }
