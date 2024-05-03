0c4fc436f107ba7c9cc92105df3d5b63d2e773c8
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-03 16:26:12 +0900

    Title: 최고의 13위치, Time: 176ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxCount = 0;
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n - 2; j++) {
+                maxCount = Math.max(maxCount, arr[i][j] + arr[i][j+1] + arr[i][j+2]);
+            }
+        }
+        System.out.println(maxCount);
+    }
+}
