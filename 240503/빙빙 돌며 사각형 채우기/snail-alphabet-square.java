e33e2d108f6a05fa999b298dd2be1e802ba1d628
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-03 12:07:15 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 227ms, Memory: 12MB, Status: Passed - CodeTree

--- 240503/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
+++ 240503/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.util.*;
 public class Main {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0 ,1, 0, -1};
     static int dir = 0;
-
+    static String[] Arrs;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+        Arrs = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
@@ -16,24 +18,25 @@ public class Main {
 
     private static void simul(char[][] arr, int n, int m) {
         int size = n * m;
-        char mark = 'A';
+        int mark = 0;
         int x = 0;
         int y = 0;
         while (size-- != 0) {
-            arr[y][x] = mark++;
+            arr[y][x] = Arrs[mark].charAt(0);
+            mark = (mark + 1) % 26;
             int nx = x + dx[dir];
-            int ny = y + dy[dir];   
+            int ny = y + dy[dir];
             if (inRange(nx, ny, n, m) || arr[ny][nx] != 0) {
                 dir = (dir + 1) % 4;
             }
-            
+
             y += dy[dir];
             x += dx[dir];
         }
     }
 
     private static boolean inRange(int x, int y, int n, int m) {
-        return x < 0 || x >= m || y < 0 || y >= n; 
+        return x < 0 || x >= m || y < 0 || y >= n;
     }
 
     private static void print(char[][] arr) {
