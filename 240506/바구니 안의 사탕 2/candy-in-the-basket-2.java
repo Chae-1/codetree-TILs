0ac1da5296d1ebcc07ac9689d8897acc318a2b7b
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-06 20:03:21 +0900

    Title: 바구니 안의 사탕 2, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240506/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240506/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
-    static int MAX_N = 401;
+    static int MAX_N = 101;
     static int[] pos = new int[MAX_N];
     
     public static void main(String[] args) {
@@ -17,10 +17,12 @@ public class Main {
         // 2 5 8
         // i  ~ i + 2k
         int max = 0;
-        for (int i = 1; i < MAX_N - 2 * k; i++) {
+        for (int i = 0; i < MAX_N; i++) {
             int sum = 0;
-            for (int j = i; j <= i + 2 * k; j++) {
-                sum += pos[j];
+            for (int j = i - k; j <= i + k; j++) {
+                if (j > 0 && j < MAX_N) {
+                    sum += pos[j];
+                }
             }
             max = Math.max(sum, max);
         }
