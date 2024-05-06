54aae11e41fea9d0446e17c7543fee4f8d90924d
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-06 12:32:06 +0900

    Title: 최고의 13위치 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240506/최고의 13위치 2/best-place-of-13-2.java
+++ 240506/최고의 13위치 2/best-place-of-13-2.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 public class Main {
-    // 8방향으로 LEE라는 문자열이 나오는 횟수를 구하라.
-    // L이 등장하는 위치에서 8방향으로 탐색
-
-    // 오른쪽 위 대각선,
-    // 오른쪽 아래 대각선
-    // 왼쪽 위 대각선
-    // 왼쪽 아래 대각선
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -21,9 +14,13 @@ public class Main {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 2; j++) {
-                for (int k = i + 1; k < n; k++) {
+                int pick1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                for (int k = i; k < n; k++) {
                     for (int l = 0; l < n - 2; l++) {
-                        max = Math.max(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2], max);
+                        if (k == i && l == j) {
+                            continue;
+                        }
+                        max = Math.max(pick1 + arr[k][l] + arr[k][l + 1] + arr[k][l + 2], max);
                     }
                 }
             }
