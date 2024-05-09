c4e4a3ffd5b6cb72728cc2f4ae3315a1adf9fa19
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-10 08:52:36 +0900

    Title: 3개의 선 2, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240510/3개의 선 2/three-lines-2.java
+++ 240510/3개의 선 2/three-lines-2.java
@@ -11,7 +11,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Point[] points = new Point[n];
