29dcbf2546e293596db79e68756a95d1acb4d9e2
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-10 08:56:13 +0900

    Title: 3개의 선 2, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240510/3개의 선 2/three-lines-2.java
+++ 240510/3개의 선 2/three-lines-2.java
@@ -28,8 +28,8 @@ public class Main {
 
         for (int i = 0; i <= 10; i++) {
             boolean allMatch = true;
-            for (int j = i + 1; j <= 10; j++) {
-                for (int k = j + 1; k <= 10; k++) {
+            for (int j = 0; j <= 10; j++) {
+                for (int k = 0; k <= 10; k++) {
                     /**
                      * 경우의 수는 4가지이다.
                      * 1. x축과 평행한 직선 3개
