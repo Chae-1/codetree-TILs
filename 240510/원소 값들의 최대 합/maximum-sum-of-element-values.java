a80d73e40a2bcb35954b7fc2acfe964050d0a0ff
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-10 14:01:12 +0900

    Title: 원소 값들의 최대 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240510/원소 값들의 최대 합/maximum-sum-of-element-values.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        // 시작하는 위치
+        int max = 0;
+        for (int start = 1; start <= n; start++) {
+
+            int cur = start;
+            int sum = 0;
+            for (int jump = 0; jump < m; jump++) {
+                cur = arr[cur];
+                sum += arr[cur];
+            }
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
