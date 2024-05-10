f9b26aea1e62f7585e05939b4fa023d64ef7fff7
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-10 10:46:32 +0900

    Title: 팀으로 하는 틱택토 2, Time: 336ms, Memory: 15MB, Status: Passed - CodeTree

--- 240510/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 240510/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.util.*;
 
 public class Main {
     static int[][] tictac = new int[3][3];
-
+ 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,76 +16,7 @@ public class Main {
         int winCount = 0;
         for (int i = 1; i <= 9; i++) {
             for (int j = i + 1; j <= 9; j++) {
-                int iMatchCounts = 0;
-                int jMatchCounts = 0;
-                // 1. 가로 3개 확인
-                for (int k = 0; k < 3; k++) {
-                    iMatchCounts = 0;
-                    jMatchCounts = 0;
-                    for (int l = 0; l < 3; l++) {
-                        if (i == tictac[k][l]) {
-                            iMatchCounts++;
-                        }
-
-                        if (j == tictac[k][l]) {
-                            jMatchCounts++;
-                        }
-                    }
-
-                    if (isWin(jMatchCounts, iMatchCounts)) {
-                        winCount++;
-                    }
-                }
-
-                // 2. 세로 3개 확인
-                for (int k = 0; k < 3; k++) {
-                    iMatchCounts = 0;
-                    jMatchCounts = 0;
-                    for (int l = 0; l < 3; l++) {
-                        if (i == tictac[l][k]) {
-                            iMatchCounts++;
-                        }
-
-                        if (j == tictac[l][k]) {
-                            jMatchCounts++;
-                        }
-                    }
-
-                    if (isWin(jMatchCounts, iMatchCounts)) {
-                        winCount++;
-                    }
-                }
-
-                // 3. 대각선 두개 확인
-                iMatchCounts = 0;
-                jMatchCounts = 0;
-                for (int k = 0; k < 3; k++) {
-                    if (tictac[k][k] == i) {
-                        iMatchCounts++;
-                    }
-
-                    if (tictac[k][k] == j) {
-                        jMatchCounts++;
-                    }
-                }
-
-                if (isWin(iMatchCounts, jMatchCounts)) {
-                    winCount++;
-                }
-
-                iMatchCounts = 0;
-                jMatchCounts = 0;
-                for (int k = 0; k < 3; k++) {
-                    if (tictac[k][2 - k] == i) {
-                        iMatchCounts++;
-                    }
-
-                    if (tictac[k][2 - k] == j) {
-                        jMatchCounts++;
-                    }
-                }
-
-                if (isWin(iMatchCounts, jMatchCounts)) {
+                if (isWin(i, j)) {
                     winCount++;
                 }
             }
@@ -92,7 +24,83 @@ public class Main {
         System.out.println(winCount);
     }
 
-    private static boolean isWin(int jMatchCounts, int iMatchCounts) {
+    private static boolean isWin(int i, int j) {
+        int iMatchCounts;
+        int jMatchCounts;
+        for (int k = 0; k < 3; k++) {
+            iMatchCounts = 0;
+            jMatchCounts = 0;
+            for (int l = 0; l < 3; l++) {
+                if (i == tictac[k][l]) {
+                    iMatchCounts++;
+                }
+
+                if (j == tictac[k][l]) {
+                    jMatchCounts++;
+                }
+            }
+
+            if (isWinCondition(jMatchCounts, iMatchCounts)) {
+                return true;
+            }
+        }
+
+        // 2. 세로 3개 확인
+        for (int k = 0; k < 3; k++) {
+            iMatchCounts = 0;
+            jMatchCounts = 0;
+            for (int l = 0; l < 3; l++) {
+                if (i == tictac[l][k]) {
+                    iMatchCounts++;
+                }
+
+                if (j == tictac[l][k]) {
+                    jMatchCounts++;
+                }
+            }
+
+            if (isWinCondition(jMatchCounts, iMatchCounts)) {
+                return true;
+            }
+        }
+
+        // 3. 대각선 두개 확인
+        iMatchCounts = 0;
+        jMatchCounts = 0;
+        for (int k = 0; k < 3; k++) {
+            if (tictac[k][k] == i) {
+                iMatchCounts++;
+            }
+
+            if (tictac[k][k] == j) {
+                jMatchCounts++;
+            }
+        }
+
+        if (isWinCondition(iMatchCounts, jMatchCounts)) {
+            return true;
+        }
+
+        iMatchCounts = 0;
+        jMatchCounts = 0;
+        for (int k = 0; k < 3; k++) {
+            if (tictac[k][2 - k] == i) {
+                iMatchCounts++;
+            }
+
+            if (tictac[k][2 - k] == j) {
+                jMatchCounts++;
+            }
+        }
+
+        if (isWinCondition(iMatchCounts, jMatchCounts)) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    private static boolean isWinCondition(int jMatchCounts, int iMatchCounts) {
         return jMatchCounts > 0 && iMatchCounts > 0 && iMatchCounts + jMatchCounts == 3;
     }
 }
