483904dc0b9671218ec3b5731644d8f62d9d2cd6
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-10 11:51:53 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 178ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240510/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
+++ 240510/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -6,18 +6,25 @@ public class Main {
         String s = sc.next();
 
         int minLen = 0;
-        //시작 위치 인덱스가 무조건 0일때
+        // 부분 문자열의 길이 1 ~ n
         for (int i = 1; i <= n; i++) {
-            String substring = s.substring(0, i);
-            int matchCount = 0;
-            for (int j = i; j <= n - i; j++) {
-                String compString = s.substring(j, j + i);
-                if (substring.equals(compString)) {
-                    matchCount++;
+            boolean anyMatch = false;
+
+            // 시작 위치를 지정
+            for (int start = 0; start <= n - i; start++) {
+                // 시작 위치에서 시작하는 부분 문자열
+                String substring = s.substring(start, start + i);
+                // 부분 문자열의 마지막 인덱스부터 확인한다.
+                for (int j = start + i; j <= n - i; j++) {
+                    String matchString = s.substring(j, j + i);
+                    if (substring.equals(matchString)) {
+                        anyMatch = true;
+                    }
                 }
             }
-
-            if (matchCount == 0) {
+            // 단 하나도 매칭되지 않았다면
+            // 최소 길이가 i
+            if (!anyMatch) {
                 minLen = i;
                 break;
             }
