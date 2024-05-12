6d2cf421e4ac932453364a900449ffa22842b8d0
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-13 08:25:06 +0900

    Title: 숫자들의 최대 차, Time: 193ms, Memory: 13MB, Status: Passed - CodeTree

--- 240513/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 240513/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -14,16 +14,25 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        // 가장 작은 값
-        int min = arr[0];
-        int count = 0;
+
+        int maxCount = 0;
+        // 시작할 작은 값을 선택한 이후
+        // 최대 차이가 k가 나는 원소들의 개수가 가장 많은 시점을 구한다.
+
         for (int i = 0; i < n; i++) {
-            // 최대 최소의 차이가 k 이하가 될 수 있는 갯수를 구한다.
-            int currMax = arr[i];
-            if (min + k >= currMax) {
-                count++;
+
+            int min = arr[i];
+            int count = 0; // i번쨰 원소를 최솟값으로 한 구간을 탐색
+            for (int j = i; j < n; j++) {
+                // 최대 최소의 차이가 k 이하가 될 수 있는 갯수를 구한다.
+                int currMax = arr[j];
+                if (min + k >= currMax) {
+                    count++;
+                }
             }
+            maxCount = Math.max(maxCount, count);
         }
-        System.out.println(count);
+        System.out.println(maxCount);
+
     }
 }
