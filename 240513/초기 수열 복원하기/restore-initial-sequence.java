4e057135594347313871b151a8053ad2c3d4666e
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-13 11:53:56 +0900

    Title: 초기 수열 복원하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240513/초기 수열 복원하기/restore-initial-sequence.java
+++ 240513/초기 수열 복원하기/restore-initial-sequence.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     static int[] a;
+    static int[] result;
+    static int n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
 
         if (n == 1) {
             // n == 1이면 수열은 한개 존재한다.
             // n을 가지는 수열 하나
             System.out.println(n);
-            return ;
+            return;
         }
 
         a = new int[n - 1];
@@ -20,36 +22,41 @@ public class Main {
             a[i] = sc.nextInt();
         }
 
-        int[] result = new int[n];
+        result = new int[n];
         for (int i = n; i >= 1; i--) {
             // 첫번째 숫자를 결정할 수 있다면
             // 그 뒤의 조합을 확인 할 수 있다.
-            boolean isEnd = true;
-            boolean[] alreadyInSequence = new boolean[n + 1];
-            if (a[0] > i) {
-                result[0] = i;
-                result[1] = a[0] - result[0];
-                for (int j = 2; j < n; j++) {
-                    int candidate = a[j - 1] - result[j - 1];
-                    // 숫자들이 단 한번 씩만 등장한다.
-                    // 등장하는 숫자를 확인하면 끝
-                    if (candidate > 0 && alreadyInSequence[candidate]) {
-                        alreadyInSequence[candidate] = true;
-                        result[j] = candidate;
-                    } else {
-                        isEnd = false;
-                        break;
-                    }
+            if (isPossible(i)) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(result[j] + " ");
                 }
-
-                if (isEnd) {
-                    break;
-                }
+                return;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(result[i] + " ");
+
+    }
+
+    private static boolean isPossible(int first) {
+        if (a[0] <= first) {
+            return false;
         }
+        boolean[] alreadyInSequence = new boolean[n + 1];
+        result[0] = first;
+        result[1] = a[0] - result[0];
+        alreadyInSequence[result[0]] = true;
+        alreadyInSequence[result[1]] = true;
+        for (int j = 2; j < n; j++) {
+            int candidate = a[j - 1] - result[j - 1];
+            // 숫자들이 단 한번 씩만 등장한다.
+            // 등장하는 숫자를 확인하면 끝
+            if (candidate > 0 && !alreadyInSequence[candidate]) {
+                alreadyInSequence[candidate] = true;
+                result[j] = candidate;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
