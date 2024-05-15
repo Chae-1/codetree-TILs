0db157170840a15daca239c50788f8dec7c09e7c
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-15 17:48:26 +0900

    Title: 전부 겹치는 선분, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240515/전부 겹치는 선분/overlapping-line-segments.java
+++ 240515/전부 겹치는 선분/overlapping-line-segments.java
@@ -10,25 +10,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        int maxX1 = Integer.MIN_VALUE;
+        int minX2 = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
+            maxX1 = Math.max(maxX1, x1[i]);
+            minX2 = Math.min(minX2, x2[i]);
         }
 
-        System.out.println(allOverlapped() ? "Yes" : "No");
-    }
-
-    public static boolean allOverlapped() {
-        for (int i = 0; i < n; i++) {
-            int baseX1 = x1[i];
-            int baseX2 = x2[i];
-            for (int j = i + 1; j < n; j++) {
-                if (baseX2 < x1[j]) return false;
-                if (baseX1 > x2[j]) return false;
-            }
+        // 어느 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 오면
+        // 겹칠 수 있는 지점이 없다.
+        if (maxX1 < minX2) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
-        
-        return true;
     }
 
 }
