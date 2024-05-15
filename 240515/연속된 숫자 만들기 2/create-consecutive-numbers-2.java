7ea43cb5a07d448086ee057b9f169f0fbcfc94e5
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-15 21:21:30 +0900

    Title: 연속된 숫자 만들기 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240515/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240515/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,27 +15,41 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             x[i] = sc.nextInt();
         }
-        // 1. A -> B로 직접 갔을 경우
-        // 1. A위치에서 x를 경유해 y로 간다음 B로 가는 경우
+        // 연속된 숫자라면 1 - 2 - 3
+        // 4 5 6
+        // 11 12 13
+        // 14 15 16
+        //
 
         System.out.println(calMinCost());
     }
 
     private static int calMinCost() {
-        int minCost = Integer.MAX_VALUE;
-        for (int i = 0; i < 3; i++) {
-            int cost = 0;
-            for (int j = 0; j < 3; j++) {
-                 if (i == j)
-                     continue;
-                 // a - b 사이에 숫자 하나를 추가로 끼울 수 있다.
-                // 그래서 차이가 2를 넘어서면 이동시켜야한다.
-                 if (Math.abs(x[i] - x[j]) > 2) {
-                     cost++;
-                 }
-            }
-            minCost = Math.min(minCost, cost);
+        // 1. 연속된 수라면
+        Arrays.sort(x);
+        int avg = (x[0] + x[2]) / 2;
+        if (avg == x[1]) {
+            return 0;
         }
-        return minCost;
+        for (int i = 0; i < 3; i++) {
+            int cur = x[i];
+            int min = Math.min(x[(i + 1) % 3], x[(i + 2) % 3]);
+            int max = Math.max(x[(i + 1) % 3], x[(i + 2) % 3]);
+
+            // cur를 구간안으로 편입
+            // max보다 클 경우, max - 1
+            // min보다 작을 경우, min + 1
+            if (cur > max) {
+                cur = max - 1;
+            } else if (cur < min) {
+                cur = min + 1;
+            }
+
+            // 조정 후, 연속된 수인지 확인
+            if (cur - min == 1 && max - cur == 1) {
+                return 1;
+            }
+        }
+        return 2;
     }
 }
