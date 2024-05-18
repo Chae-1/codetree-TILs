da75c9fd8540a23964f483a202015d06f3a5cdc1
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-19 00:12:58 +0900

    Title: 독서실의 거리두기 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    static char a[];
+
+    private static int getMinDistBetweenTwo(int n) {
+        int prev = 0;
+        int dist = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == '1') {
+                if (a[prev] == '1') {
+                    dist = Math.min(i - prev, dist);
+                }
+                prev = i;
+            }
+        }
+        return dist;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        a = sc.next().toCharArray();
+
+        // 1. 1의 쌍의 거리가 가장 긴 위치를 찾자.
+        int maxDistBetweenTwo = Integer.MIN_VALUE;
+        int longI = 0;
+        int longJ = 0;
+        int lastOnePos = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == '1') {
+                // 가장 가까운 1의 쌍을 찾는다.
+                for (int j = i + 1; j < n; j++) {
+                    if (a[j] == '1') {
+                        // 쌍을 찾았다.
+                        // 이 두 자리의 위치를 기억한다.
+                        // 이전 거리보다 최대여야 한다.
+                        int dist = j - i; // 거리
+                        if (dist > maxDistBetweenTwo) {
+                            longI = i;
+                            longJ = j;
+                            maxDistBetweenTwo = dist;
+                        }
+                        break;
+                    }
+                }
+                // 가장 마지막에 등장한 1의 위치를 기억한다.
+                lastOnePos = i;
+            }
+        }
+        int mid = (longI + longJ) / 2;
+        a[mid] = '1';
+        int minDistBetweenTwo = getMinDistBetweenTwo(n);
+        a[mid] = '0';
+        // 마지막 위치가 0이라면, 그 자리에 1을 두고 비교한다.
+        // 13 - 8 == 5
+        if (a[n - 1] == '0' && (n - 1) - lastOnePos > maxDistBetweenTwo / 2) {
+            minDistBetweenTwo = Math.max(minDistBetweenTwo, n - 1 - lastOnePos);
+        }
+        System.out.println(minDistBetweenTwo);
+    }
+}
