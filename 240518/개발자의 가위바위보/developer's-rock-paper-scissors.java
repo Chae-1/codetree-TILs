3d09e4ce4889b9e56215c74c187f257069199ccf
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-18 22:29:36 +0900

    Title: 개발자의 가위바위보, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240518/개발자의 가위바위보/developer's-rock-paper-scissors.java
+++ 240518/개발자의 가위바위보/developer's-rock-paper-scissors.java
@@ -6,11 +6,6 @@ public class Main {
     // a가 b를 이길 수 있는 최대 값
     static int a[] = new int[MAX_N];
     static int b[] = new int[MAX_N];
-    // 가위 > 바위 > 보
-    // 바위 > 가위 > 보
-    //
-    // 보 > 바위 > 가위
-    // 1    2      3
 
 
     public static void main(String[] args) {
@@ -25,24 +20,20 @@ public class Main {
         // 같으면 비긴다. 절대값 차이가 1 차이 날 떄 이기는 경우 2 차이가 날때
         int maxCount = 0;
         for (int i = 1; i <= 3; i++) {
-            // 작다, 크다
-            // 1 2 3
-            // 차이가 2가 날때 이기는 경우
             int diffTwo = 0;
-            // 차이가 1이 날때 이기는 경우
             int diffOne = 0;
             for (int j = 0; j < n; j++) {
-                int afterOne = (a[j] + 1) % 3 == 0 ? 1: (a[j] + 1) % 3;
-                if (afterOne == b[j]) {
+                int diff = a[j] + 1 >= 4 ? 1 : a[j] + 1;
+                if (diff == b[j]) {
                     diffOne++;
                 }
-
-                int afterTwo = (a[j] + 2) % 3 == 0 ? 1 : (a[j] + 2) % 3;
-                if (afterTwo == b[j]) {
+                // 3 + 2 -> 2
+                int diff2 = a[j] + 2 >= 4 ? (a[j] + 2) % 4 + 1 : a[j] + 2;
+                if (diff2 == b[j]) {
                     diffTwo++;
                 }
             }
-            maxCount = Math.max(diffTwo, diffOne);
+            maxCount = Math.max(maxCount, Math.max(diffTwo, diffOne));
         }
         System.out.println(maxCount);
     }
