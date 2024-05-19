a16533b361eefcb5d7b4689aa5675ad6a5d4a10a
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-19 13:28:09 +0900

    Title: ABC 줄 세우기, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/ABC 줄 세우기/abc-line-up.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] arr = new char[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next().charAt(0);
+        }
+
+        char[] source = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arr);
+        for (char c : source) {
+            System.out.println(c);
+        }
+        // A C D B
+        //   B를 위치시켜주는 최선
+        //
+        int dist = 0;
+        // A
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == source[j]) {
+                    if (i != j) {
+                        dist += Math.abs(j - i);
+                        swap(source, i, j);
+                    }
+                }
+            }
+        }
+        System.out.println(dist);
+    }
+
+    private static void swap(char[] source, int i, int j) {
+        char temp = source[i];
+        source[i] = source[j];
+        source[j] = temp;
+    }
+}
