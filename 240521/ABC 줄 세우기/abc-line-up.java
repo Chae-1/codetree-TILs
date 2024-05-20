3282fac0510da895448fe5e683d63e16b406b1fe
Chae-1 <59527788+Chae-1@users.noreply.github.com>
2024-05-21 08:46:48 +0900

    Title: ABC 줄 세우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240521/ABC 줄 세우기/abc-line-up.java
+++ 240521/ABC 줄 세우기/abc-line-up.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     static char[] source;
-    static char[] sorted;
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -13,32 +13,17 @@ public class Main {
             source[i] = sc.next().charAt(0);
         }
 
-        sorted = Arrays.copyOf(source, n);
-        Arrays.sort(sorted);
-
         // 맨 뒤에서 부터 자기 순서를 찾아가면, 저절로 제자리를 찾아 갈 것이다
+        // 역순 쌍의 개수를 구하면 된다.
         int count = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-
-                if (source[j] == sorted[i]) {
-                    if (j != i) {
-                        // j != i 라면
-                        // i - j번 스왑하면 된다.
-                        for (int k = j; k < n - 1; k++) {
-                            swap(k, k+1);
-                            count++;
-                        }
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (source[i] > source[j]) {
+                    count++;
                 }
             }
         }
         System.out.println(count);
-    }
+    }}
 
-    private static void swap(int j, int i) {
-        char temp = source[i];
-        source[i] = source[j];
-        source[j] = temp;
-    }
 }
